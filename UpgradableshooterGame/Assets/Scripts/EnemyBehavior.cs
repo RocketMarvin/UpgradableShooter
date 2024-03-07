044756cefd012188c320511df22ffc707b25cef9
RocketMarvin <jonkermarvin1@gmail.com>
2024-03-07 15:24:15 +0100

    Target
    moet nog af

---
+++ UpgradableshooterGame/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public GameObject currentTarget;
+    public GameObject target1;
+    public GameObject target2;
+   public void TargetSwitch()
+   {
+
+   }
+    public void Target1()
+    {
+       
+    }
+    public void Target2()
+    {
+        
+    }
+
+}
