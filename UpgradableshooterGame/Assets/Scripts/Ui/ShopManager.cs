1342227dae27c2f7e51101f045af6cf0f6176d87
RocketMarvin <jonkermarvin1@gmail.com>
2024-04-03 14:25:46 +0200

    Shop anim 0.5

--- UpgradableshooterGame/Assets/Scripts/Ui/ShopManager.cs
+++ UpgradableshooterGame/Assets/Scripts/Ui/ShopManager.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
     public int coins;
-    public TMP_Text cointText;
-    public ShopItemSD[] shopitemSD;
+    public TMP_Text coinsText;
+    public ShopItemSD[] shopitemSO;
+    public GameObject[] shopPanelsGO;
     public ShopTemplate[] shopPanels;
+    public Button[] myPurchaseButtons;
      
     // Start is called before the first frame update
     void Start()
     {
-        cointText.text = "Coins: " + coins.ToString();
+        for (int i = 0; i < shopitemSO.Length; i++)
+            shopPanelsGO[i].gameObject.SetActive(true);
+        coinsText.text = "Coins: " + coins.ToString();
+        LoadPanels();
+        CheckPurchaseable();
     }
 
     // Update is called once per frame
@@ -22,20 +29,44 @@ public class ShopManager : MonoBehaviour
         
     }
 
+    public void CheckPurchaseable()
+    {
+        for (int i = 0; i < shopitemSO.Length; i++)
+        {
+            if (coins >= shopitemSO[i].basecost)
+            {
+                myPurchaseButtons[i].interactable = true;
+            }
+            else
+            {
+                myPurchaseButtons[i].interactable = false;
+            }
+        }
+    }
+    public void PurchaseItem(int btnNo)
+    {
+        if (coins >= shopitemSO[btnNo].basecost)
+        {
+            coins =  coins - shopitemSO[btnNo].basecost;
+            coinsText.text = "Coins: " + coins.ToString();
+            CheckPurchaseable();
+        }
+    }
+
     public void AddCoins()
     {
         coins++;
-        cointText.text = "Coins: " + coins.ToString();
-        
+        coinsText.text = "Coins: " + coins.ToString();
+        CheckPurchaseable();
     }
 
     public void LoadPanels()
     {
-        for (int i = 0; i < shopitemSD.Length; i++)
+        for (int i = 0; i < shopitemSO.Length; i++)
         {
-            shopPanels[i].titleText.text = shopitemSD[i].title;
-            shopPanels[i].descriptionText.text = shopitemSD[i].description;
-            shopPanels[i].priceText.text = "Coins" + shopitemSD[i].basecost.ToString();
+            shopPanels[i].titleText.text = shopitemSO[i].title;
+            shopPanels[i].descriptionText.text = shopitemSO[i].description;
+            shopPanels[i].priceText.text = "Coins " + shopitemSO[i].basecost.ToString();
         }
     }
 }
